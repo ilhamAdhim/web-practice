3530889c1845ad838f56d09840946d7ff3a710da
ilhamAdhim <ilhamm179@gmail.com>
2019-03-17 16:15:14 +0700

    Login and get data from server.py -> 2nd Activity in second day of Flask

--- server.py
+++ server.py
@@ -1,8 +1,16 @@
 #import flask
-from flask import Flask ,jsonify ,request,render_template, url_for
-
+# render_template digunakan untuk merender html dan css . 
+# url_for digunakan di file layout.html untuk mencari file css
+from flask import Flask ,jsonify ,request,render_template, url_for , flash , redirect
+from forms import RegistrationForm , LoginForm
 #membuat aplikasi flask 	 	
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = '3ac9f1844e2e16c02698d86c05047fde'
+
+
+
 biodata = [	
     {
         "nama": "budi",
@@ -44,5 +52,29 @@ def about():
 @app.route("/biodata")
 def index():
     return jsonify(biodata)
+
+
+@app.route("/register" , methods = ['GET','POST'])
+def register():
+  form = RegistrationForm()
+
+  if form.validate_on_submit():
+  		
+  		flash(f'Account created for { form.username.data }! ' , 'success')
+  		return redirect('home')
+
+# below Python 3.6 use this one :
+# flash('Account created for {}!'.format({form.username.data}))
+
+  	
+  return render_template('register.html' , title='Register' , form = form)
+
+
+@app.route("/login")
+def login():
+  Loginform = LoginForm()
+  return render_template('login.html')
+
+
 
 app.run()
