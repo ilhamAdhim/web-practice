65fe638ff18de37c599f026e9b8e7ca9c05d15ca
ilhamAdhim <ilhamm179@gmail.com>
2019-04-07 14:03:31 +0700

    add UpdateAccountForm

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,8 @@
 from flask_wtf import FlaskForm
+# fileField is a field to export file from local desktop
+# File allowed is a validator what kind of file allowed uploaded
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -7,13 +11,10 @@ from flaskblog.models import User
 class RegistrationForm(FlaskForm):
     # this is used to make a limit of username length, 
     # if the condition isn't match then user can't continue
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    check_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+    check_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
@@ -28,8 +29,29 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+    email = StringField('Email',validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+# This class is used to make update in account.html
+# it includes updating uname , email , and profile picture
+class UpdateAccountForm(FlaskForm):
+    # this is used to make a limit of username length, 
+    # if the condition isn't match then user can't continue
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png','jpeg'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
