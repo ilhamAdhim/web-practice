cb13255292cce683b9a9109363c8902470579ebc
ilhamAdhim <ilhamm179@gmail.com>
2019-04-07 14:04:09 +0700

    update profile picture of logged in user

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,8 @@
+import secrets , os
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm , UpdateAccountForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -38,6 +40,7 @@ def register():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
 
+        # Takes RegistrationForm from forms.py
     form = RegistrationForm()
 
     if form.validate_on_submit():
@@ -83,9 +86,53 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route("/account")
+def save_picture(form_picture):
+    # we want to make a hex random to the saved picture from user just to avoid
+    # collision if the original name is already in our folder
+    random_hex = secrets.token_hex(8)
+    # pass in the picture that user uploaded to parameter
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+
+    # Move the picture uploaded to our storage
+    picture_path = os.path.join(app.root_path,'static/profile_pics/', picture_fn)
+
+    # from pillow modules, resize the uploaded image to 125 px
+    # resize, so that we can make our website faster to load and save storages
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    # save picture to picture_path directory (earlier)
+    # form_picture.save(picture_path)
+    i.save(picture_path)
+
+    return picture_fn
+
+@app.route("/account",methods=['GET', 'POST'])
 # login_required is a modules from flask-login
 # below this comment, is used to make user logged in first before they can access account.html
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
+
+        # change the username and email of logged in user 
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        # not use session.add because we want to change database , not add a new record
+        db.session.commit()
+        flash('Your account has been updated' , 'success')
+        # reason why we put redirect to account here is to prevent unsaved changes of data when we render
+        # account.html again. bcs it will send new get request
+        return redirect(url_for('account'))
+
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    image_file = url_for('static',filename = 'profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file = image_file , form = form)
